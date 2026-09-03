6ca4b93f48424afbdc558bd68572761124fbbade
Hermon Haileyesus <hermongeb2004@gmail.com>
2026-09-03 22:40:21 +0300

    Refactor HomeScreen component to use ScrollView and global styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,12 @@
-import { Text, View, StyleSheet } from "react-native";
+import Header from "@/components/Header";
+import { globalStyles } from "@/css/styles";
+import { ScrollView, Text } from "react-native";
 
-export default function Index() {
+export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>PlanSum</Text>
+      <Header />
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
