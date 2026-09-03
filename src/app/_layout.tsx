b37f136dea7ca98b6c24c16c382af2bab810dfb5
Hermon Haileyesus <hermongeb2004@gmail.com>
2026-09-03 23:31:08 +0300

    call tab and use its layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,9 @@
-import { colors } from "@/css/styles";
 import { Stack } from "expo-router";
+
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: colors.header },
-        headerTintColor: "#fff",
-      }}
-    >
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="plans" options={{ title: "plans" }} />
-      <Stack.Screen name="add-plan" options={{ title: "Add Plan" }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
     </Stack>
   );
 }
