a86970e7b25c9bc4609ac0ec11ac5723f24581c7
Hermon Haileyesus <hermongeb2004@gmail.com>
2026-09-03 23:05:28 +0300

    Refactor RootLayout to include header styles and update HomeScreen to add navigation link to Plans

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,16 @@
+import { colors } from "@/css/styles";
 import { Stack } from "expo-router";
-
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: colors.header },
+        headerTintColor: "#fff",
+      }}
+    >
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="plans" options={{ title: "plans" }} />
+      <Stack.Screen name="add-plan" options={{ title: "Add Plan" }} />
+    </Stack>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,16 @@
 import Header from "@/components/Header";
 import { globalStyles } from "@/css/styles";
+import { Link } from "expo-router";
 import { ScrollView, Text } from "react-native";
-
 export default function HomeScreen() {
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>PlanSum</Text>
       <Header />
+
+      <Link href="/plans" style={{ fontSize: 18, color: "#007bff" }}>
+        Go to Plans
+      </Link>
     </ScrollView>
   );
 }
